46ce29daf60cbb427e3185319bb4722d99b99119
morshedulmunna <morshedulmunna1@gmail.com>
2023-02-25 08:53:33 +0600

    create ticker users feedback prisma model and tickts CURD operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [TicketsModule],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
