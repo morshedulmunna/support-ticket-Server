bbedd3af458a5b60357f19c370031c4c3a95085d
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-08 20:13:41 +0600

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -12,15 +11,6 @@ async function bootstrap() {
   // Validation Pipe Global
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(4000);
+  await app.listen(5000);
 }
 bootstrap();
