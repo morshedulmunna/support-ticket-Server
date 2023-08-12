ae9ef6bbf5b12b6e2c9c2a8c3f265bb24cc7b316
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-12 19:32:38 +0600

    cors open

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // Cors Enable
-  app.enableCors();
-
+  // Enable CORS for specific origins
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   // Validation Pipe Global
   app.useGlobalPipes(new ValidationPipe());
 
